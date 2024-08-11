1637d07b3541dd1647dcc3e475c5aed4004f9c08
mjmoshiri <thisismjmoshiri@gmail.com>
2024-08-11 13:17:02 -0700

    Add dynamic render and routing

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import styles from "./layout.module.css";
+import Head from "next/head";
+import Image from "next/image";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'This is Todd Chavez👆'
-export const siteTitle = 'Next.js Sample Website'
+const name = "This is Todd Chavez 👆";
+export const siteTitle = "Next.js Sample Project";
 
 export default function Layout({ children, home }) {
   return (
@@ -14,14 +14,9 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Learn how to build a blog with Next Js"
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta property="og:image" content={``} />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
@@ -47,7 +42,7 @@ export default function Layout({ children, home }) {
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
-                alt={name}
+                alt=""
               />
             </Link>
             <h2 className={utilStyles.headingLg}>
@@ -61,9 +56,9 @@ export default function Layout({ children, home }) {
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
+          <Link href="/">Back to home</Link>
         </div>
       )}
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Link from "next/link";
+import Date from '../components/date';
+import Layout, { siteTitle } from "../components/layout";
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
 
 export default function Home({ allPostsData }) {
+  {console.log('allPostsData:' , allPostsData)}
   return (
     <Layout home>
       <Head>
@@ -13,28 +25,21 @@ export default function Home({ allPostsData }) {
         <p>He is like a toddler. But a Todd version: a Toddler!</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}
+        style={{color:'green'}}
+        >Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
-  )
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
+  );
 }
